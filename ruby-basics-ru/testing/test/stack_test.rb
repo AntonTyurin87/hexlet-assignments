53b65a410492d4bd1f16b03831ac69b60bc52325
Антон Тюрин <AntonTyurin87@gmail.com>
2023-11-08 17:23:22 +0300

    Start doing first exersises

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -5,7 +5,34 @@ require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
-  
+  def setup; end
+
+  def teardown; end
+
+  def test_to_a_returns_true
+    stack = Stack.new
+    assert(stack.to_a == [])
+  end
+
+  def test_empty_returns_true
+    stack = Stack.new
+    assert(stack.empty? == true)
+  end
+
+  def test_size_returns_zirro
+    stack = Stack.new
+    assert(stack.size == 0)
+  end
+
+  def test_adding_elements
+    stack = Stack.new
+    stack.push! 'ruby'
+    stack.push! 'php'
+    stack.push! 'java'
+    assert(stack.to_a == ['ruby', 'php', 'java'])
+    assert(stack.size == 3)
+    assert(stack.empty? == false)
+  end
   # END
 end
 
