a0890b6b9ae9767354698f4fbc865ba5866e44c2
@hexlet/cli <support@hexlet.io>
2023-11-14 15:07:29 +0300

    submit ruby-basics-ru/oop

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -1,42 +1,52 @@
 # frozen_string_literal: true
+require 'uri'
 
 # BEGIN
-require 'uri'
-require 'forwardable'
-
 class Url
-  
   include Comparable
-
-  def initialize(url)
-    @url = URI(url)
-  end
-
-  include Comparable
-  attr :str
-  def <=>(anOther)
-     str.size <=> anOther.str.size
-  end
-  def initialize(str)
-     @str = str
-  end
-  def inspect
-    @str
-  end
-
-  def query_params
-    ary = URI.decode_www_form(@url.query)
-    ary.to_h
-  end
-
-  def query_param(key, value = nil)
-    ary = URI.decode_www_form(@url.query)
-    ary.to_h["#{key}"] ? ary.to_h["#{key}"] : value
-  end
-
   extend Forwardable
 
-  def_delegators :@url, :scheme, :host, :port
-end
+  attr_reader :url, :query_params
 
+  def_delegator :@url, :scheme, :scheme
+  def_delegator :@url, :host, :host
+  def_delegator :@url, :to_s, :to_s
+
+  def initialize(params)
+    @url = URI(params)
+
+    @query_params = (url.query || '').split('&').each_with_object({}) do |item, acc|
+      acc[item.split('=')[0].to_sym] = item.split('=')[1]
+    end
+  end
+
+  def query_param(key, default = nil)
+    query_params.fetch(key, default)
+  end
+
+  def ==(other)
+    to_s == other.to_s
+  end
+end
 # END
+
+
+# yandex_url = Url.new 'http://yandex.ru?key=value&key2=value2'
+
+# yandex_url_same = Url.new 'http://yandex.ru?key2=value2&key=value&key3=value3'
+
+
+# puts (yandex_url == yandex_url_same) # true
+
+# puts yandex_url.inspect
+
+# puts yandex_url.scheme
+# puts yandex_url.host
+# puts yandex_url.port
+# puts yandex_url.query_params # { key: 'value', key2: 'value2' }
+# puts yandex_url.query_param(:key) # 'value'
+# puts yandex_url.query_param(:key2, 'lala') # 'value2'
+# puts yandex_url.query_param(:new, 'ehu') # 'ehu'
+# puts yandex_url.query_param(:lalala) # nil
+# puts yandex_url.query_param(:lalala, 'default') # 'default'
+# #puts yandex_url.query_params
