b72a1307d1de7759f295f1bc3751abddbd1438db
Антон Тюрин <AntonTyurin87@gmail.com>
2023-11-15 09:48:58 +0300

    Next variant of second lesson.

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -1,31 +1,37 @@
 # frozen_string_literal: true
-require 'uri'
 
 # BEGIN
+require 'uri'
+require 'forwardable'
+
 class Url
   include Comparable
   extend Forwardable
 
-  attr_reader :url, :query_params
+  attr_reader :url
 
-  def_delegator :@url, :scheme, :scheme
-  def_delegator :@url, :host, :host
-  def_delegator :@url, :to_s, :to_s
+  def_delegators :@url, :scheme, :host, :port, :query
 
   def initialize(params)
     @url = URI(params)
+    @ary = URI.decode_www_form(@url.query)
+    @url
+  end
 
-    @query_params = (url.query || '').split('&').each_with_object({}) do |item, acc|
-      acc[item.split('=')[0].to_sym] = item.split('=')[1]
+  def query_params
+    h = {}
+    @ary.each do |key, value|
+      h[key.to_sym] = value
     end
+    h
   end
 
-  def query_param(key, default = nil)
-    query_params.fetch(key, default)
+  def query_param(key, value = nil)
+    query_params[key] ? query_params[key] : value
   end
 
-  def ==(other)
-    to_s == other.to_s
+  def == (other)
+    @ary.sort == URI.decode_www_form(other.query).sort
   end
 end
 # END
@@ -33,20 +39,16 @@ end
 
 # yandex_url = Url.new 'http://yandex.ru?key=value&key2=value2'
 
-# yandex_url_same = Url.new 'http://yandex.ru?key2=value2&key=value&key3=value3'
-
+# yandex_url_same = Url.new 'http://yandex.ru?key2=value2&key=value'
 
 # puts (yandex_url == yandex_url_same) # true
 
-# puts yandex_url.inspect
-
-# puts yandex_url.scheme
-# puts yandex_url.host
-# puts yandex_url.port
+#  puts yandex_url.scheme
+#  puts yandex_url.host
+#  puts yandex_url.port
 # puts yandex_url.query_params # { key: 'value', key2: 'value2' }
 # puts yandex_url.query_param(:key) # 'value'
 # puts yandex_url.query_param(:key2, 'lala') # 'value2'
 # puts yandex_url.query_param(:new, 'ehu') # 'ehu'
 # puts yandex_url.query_param(:lalala) # nil
 # puts yandex_url.query_param(:lalala, 'default') # 'default'
-# #puts yandex_url.query_params
