d81f340fa4cb6f89ac54f4de7ba341285923b6f0
@hexlet/cli <support@hexlet.io>
2023-11-15 10:10:07 +0300

    submit ruby-basics-ru/oop

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -14,7 +14,10 @@ class Url
 
   def initialize(params)
     @url = URI(params)
-    @ary = URI.decode_www_form(@url.query)
+    #@ary = URI.decode_www_form(@url.query)
+    @ary = (@url.query || '').split('&').each_with_object({}) do |item, acc|
+      acc[item.split('=')[0].to_sym] = item.split('=')[1]
+    end
     @url
   end
 
@@ -31,11 +34,12 @@ class Url
   end
 
   def == (other)
-    @ary.sort == URI.decode_www_form(other.query).sort
+    n = (other.query || '').split('&').each_with_object({}) do |item, acc|
+      acc[item.split('=')[0].to_sym] = item.split('=')[1] end
+    @ary == n  #URI.decode_www_form(other.query).sort 
   end
 end
 # END
-
 
 # yandex_url = Url.new 'http://yandex.ru?key=value&key2=value2'
 
@@ -43,9 +47,9 @@ end
 
 # puts (yandex_url == yandex_url_same) # true
 
-#  puts yandex_url.scheme
-#  puts yandex_url.host
-#  puts yandex_url.port
+# puts yandex_url.scheme
+# puts yandex_url.host
+# puts yandex_url.port
 # puts yandex_url.query_params # { key: 'value', key2: 'value2' }
 # puts yandex_url.query_param(:key) # 'value'
 # puts yandex_url.query_param(:key2, 'lala') # 'value2'
